f3c22197cb18dc25e7be2e42c4f6a03be7fc58ad
mp2apps <mp2apps@icloud.com>
2014-02-08 18:21:47 +0100

    Update version.cpp
    New versión suffix: -MM&KWG

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-ManicMiner"
+#define CLIENT_VERSION_SUFFIX   "-MM&KGW"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
