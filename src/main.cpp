6e8173b0ccc027bc6338b67c9d876763e8e5ea04
mp2apps <mp2apps@icloud.com>
2014-04-03 16:31:09 +0200

    Update main.cpp
    Kimoto Gravity Well Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1202,6 +1202,10 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
         double                                EventHorizonDeviationSlow;
         
     if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit.GetCompact(); }
+	
+	// CryptoMP - KGW Fix
+	int64 LatestBlockTime = BlockLastSolved->GetBlockTime();
+       
         
         for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
                 if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
@@ -1211,10 +1215,32 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
                 else                { PastDifficultyAverage = ((CBigNum().SetCompact(BlockReading->nBits) - PastDifficultyAveragePrev) / i) + PastDifficultyAveragePrev; }
                 PastDifficultyAveragePrev = PastDifficultyAverage;
                 
-                PastRateActualSeconds                        = BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
+                // CryptoMP - KGW Fix - PastRateActualSeconds                        = BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
+                if (LatestBlockTime < BlockReading->GetBlockTime()) {
+
++		        if (BlockReading->nHeight > 127500)
+
++		                LatestBlockTime = BlockReading->GetBlockTime();
+
++		}
+
++		PastRateActualSeconds = LatestBlockTime - BlockReading->GetBlockTime();
+                
+                
+                
                 PastRateTargetSeconds                        = TargetBlocksSpacingSeconds * PastBlocksMass;
                 PastRateAdjustmentRatio                        = double(1);
-                if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+                // CryptoMP -KGW FIx - if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+                if (BlockReading->nHeight > 127500)
+
++		        if (PastRateActualSeconds < 1) { PastRateActualSeconds = 1; }
+
++		} else {
+
++		        if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+
++		}
+                
                 if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
                 PastRateAdjustmentRatio                        = double(PastRateTargetSeconds) / double(PastRateActualSeconds);
                 }
