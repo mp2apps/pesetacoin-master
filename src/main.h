e368433203a3a1389cb82af4d9e99dbcb3fb5da3
pesetachain <admin@pesetachain.info>
2014-12-29 18:38:10 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1416,6 +1416,7 @@ public:
         block.nTime          = nTime;
         block.nBits          = nBits;
         block.nNonce         = nNonce;
+        block.auxpow         = auxpow;
         return block;
     }
 
