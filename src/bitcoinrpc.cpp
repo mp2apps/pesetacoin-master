01fdfb0336bb43d769a819b0e5232996d0396b39
pesetachain <admin@pesetachain.info>
2015-01-10 20:01:16 +0100

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 16640 : 26640;
+    return GetBoolArg("-testnet", false) ? 16640 : 16638;
 }
 
 Object JSONRPCError(int code, const string& message)
