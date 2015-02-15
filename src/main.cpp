f996166ae07c3c33ffe009e8384f68e414778d50
pesetachain <admin@pesetachain.info>
2015-02-15 17:47:10 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1303,23 +1303,73 @@ unsigned int static GetNextWorkRequired_V2(const CBlockIndex* pindexLast, const 
 }
 
 
+unsigned int static DarkGravityWave(const CBlockIndex* pindexLast, const CBlockHeader *pblock) {
+/* current difficulty formula, darkcoin - DarkGravity v3, written by Evan Duffield - evan@darkcoin.io */
+const CBlockIndex *BlockLastSolved = pindexLast;
+const CBlockIndex *BlockReading = pindexLast;
+const CBlockHeader *BlockCreating = pblock;
+BlockCreating = BlockCreating;
+int64_t nActualTimespan = 0;
+int64_t LastBlockTime = 0;
+int64_t PastBlocksMin = 24;
+int64_t PastBlocksMax = 24;
+int64_t CountBlocks = 0;
+CBigNum PastDifficultyAverage;
+CBigNum PastDifficultyAveragePrev;
+
+if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || BlockLastSolved->nHeight < PastBlocksMin) {
+	return bnProofOfWorkLimit.GetCompact();
+}
+
+for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
+	if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
+	CountBlocks++;
+	if(CountBlocks <= PastBlocksMin) {
+		if (CountBlocks == 1) { PastDifficultyAverage.SetCompact(BlockReading->nBits); }
+		else { PastDifficultyAverage = ((PastDifficultyAveragePrev * CountBlocks)+(CBigNum().SetCompact(BlockReading->nBits))) / (CountBlocks+1); }
+		PastDifficultyAveragePrev = PastDifficultyAverage;
+	}
+	if(LastBlockTime > 0){
+		int64_t Diff = (LastBlockTime - BlockReading->GetBlockTime());
+		nActualTimespan += Diff;
+	}
+	LastBlockTime = BlockReading->GetBlockTime();
+	if (BlockReading->pprev == NULL) { assert(BlockReading); break; }
+	BlockReading = BlockReading->pprev;
+	}
+	CBigNum bnNew(PastDifficultyAverage);
+	int64_t _nTargetTimespan = CountBlocks*nTargetSpacing;
+	if (nActualTimespan < _nTargetTimespan/3)
+		nActualTimespan = _nTargetTimespan/3;
+	if (nActualTimespan > _nTargetTimespan*3)
+		nActualTimespan = _nTargetTimespan*3;
+	// Retarget
+	bnNew *= nActualTimespan;
+	bnNew /= _nTargetTimespan;
+	
+	if (bnNew > bnProofOfWorkLimit){
+		bnNew = bnProofOfWorkLimit;
+	}
+return bnNew.GetCompact();
+}
+
 unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
 {
         int DiffMode = 1;
         if (fTestNet) {
                 if (pindexLast->nHeight+1 >= 50) { DiffMode = 2; }
+                if (pindexLast->nHeight+1 >= 500) { DiffMode = 3; }
         }
         else { // CryptoMP - Kimoto Will Start at block 46.000
                 if (pindexLast->nHeight+1 >= 46000) { DiffMode = 2; }
+                if (pindexLast->nHeight+1 >= 582500) { DiffMode = 3; }
         }
         
-        if                (DiffMode == 1) { return GetNextWorkRequired_V1(pindexLast, pblock); }
+        if             (DiffMode == 1) { return GetNextWorkRequired_V1(pindexLast, pblock); }
         else if        (DiffMode == 2) { return GetNextWorkRequired_V2(pindexLast, pblock); }
+        else if        (DiffMode == 3) { return DarkGravityWave(pindexLast, pblock); }
         return GetNextWorkRequired_V2(pindexLast, pblock);
 }
-
-
-
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 {
